00dca331b350055630cf9da7090604977af9871b
Guillaume Laforge <glaforge@gmail.com>
2024-10-06 21:31:31 +0200

    Formatting

--- devoxx-rag-naive-to-advanced/src/test/java/devoxx/rag/_4_advanced_rag_query/_48_sample_function_calling.java
+++ devoxx-rag-naive-to-advanced/src/test/java/devoxx/rag/_4_advanced_rag_query/_48_sample_function_calling.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class _48_sample_function_calling extends AbstractDevoxxTest {
 
@@ -37,31 +38,33 @@ public class _48_sample_function_calling extends AbstractDevoxxTest {
         Calculator calculator = new Calculator();
 
         Assistant assistant = AiServices.builder(Assistant.class)
-                .chatLanguageModel(getChatLanguageModel(MODEL_GEMINI_PRO))
-                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
-                .tools(calculator)
-                .build();
+            .chatLanguageModel(getChatLanguageModel(MODEL_GEMINI_PRO))
+            .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+            .tools(calculator)
+            .build();
 
         String answer = assistant.chat("How much is 754 + 926?");
         System.out.println(answer);
     }
 
     @Test
-    void Low_level_Tools_Example() {
+    public void low_level_tools_example() {
 
         List<ToolSpecification> toolSpecifications = ToolSpecifications.toolSpecificationsFrom(new Calculator());
 
         UserMessage userMessage = new UserMessage("How much is 754 + 926?");
 
-        Response<AiMessage> response = getChatLanguageModel(MODEL_GEMINI_PRO).generate(singletonList(userMessage), toolSpecifications);
+        Response<AiMessage> response = getChatLanguageModel(MODEL_GEMINI_PRO)
+            .generate(singletonList(userMessage), toolSpecifications);
 
         AiMessage aiMessage = response.content();
-        Assertions.assertThat(aiMessage.hasToolExecutionRequests()).isTrue();
-        Assertions.assertThat(aiMessage.toolExecutionRequests()).hasSize(1);
+        assertThat(aiMessage.hasToolExecutionRequests()).isTrue();
+        assertThat(aiMessage.toolExecutionRequests()).hasSize(1);
 
         ToolExecutionRequest toolExecutionRequest = aiMessage.toolExecutionRequests().get(0);
-        Assertions.assertThat(toolExecutionRequest.name()).isEqualTo("add");
-        Assertions.assertThat(toolExecutionRequest.arguments()).isEqualToIgnoringWhitespace("{\"arg1\":926.0,\"arg0\":754.0}");
+        assertThat(toolExecutionRequest.name()).isEqualTo("add");
+        assertThat(toolExecutionRequest.arguments())
+            .isEqualToIgnoringWhitespace("{\"arg1\":926.0,\"arg0\":754.0}");
     }
 
 }
