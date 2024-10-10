2aa7217be5208ea4d81eeeacd5a08326fea370e6
Guillaume Laforge <glaforge@gmail.com>
2024-10-10 10:52:12 +0200

    Change multilingual embedding example

--- devoxx-rag-naive-to-advanced/src/test/java/devoxx/rag/_3_advanced_rag_ingestion/_33_2_embedding_multilingual.java
+++ devoxx-rag-naive-to-advanced/src/test/java/devoxx/rag/_3_advanced_rag_ingestion/_33_2_embedding_multilingual.java
@@ -3,6 +3,7 @@ package devoxx.rag._3_advanced_rag_ingestion;
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2QuantizedEmbeddingModel;
 import dev.langchain4j.model.output.Response;
+import dev.langchain4j.model.vertexai.VertexAiEmbeddingModel;
 import dev.langchain4j.store.embedding.CosineSimilarity;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +12,27 @@ public class _33_2_embedding_multilingual {
     @Test
     public void should_use_local_embedding_model() {
         var embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+
         Response<Embedding> embeddingOne = embeddingModel.embed("baby dog");
         Response<Embedding> embeddingTwo = embeddingModel.embed("puppy");
+
         double similarity = CosineSimilarity.between(embeddingOne.content(), embeddingTwo.content());
         System.out.println("similarity = " + similarity);
     }
 
     @Test
     public void should_use_multimodal_embedding_model() {
-        var embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+        var embeddingModel = VertexAiEmbeddingModel.builder()
+            .project(System.getenv("GCP_PROJECT_ID"))
+            .endpoint(System.getenv("GCP_VERTEXAI_ENDPOINT"))
+            .location(System.getenv("GCP_LOCATION"))
+            .publisher("google")
+            .modelName("text-embedding-004")
+            .build();;
+
         Response<Embedding> embeddingOne = embeddingModel.embed("baby dog");
-        Response<Embedding> embeddingTwo = embeddingModel.embed("puppy");
+        Response<Embedding> embeddingTwo = embeddingModel.embed("bébé chien");
+
         double similarity = CosineSimilarity.between(embeddingOne.content(), embeddingTwo.content());
         System.out.println("similarity = " + similarity);
     }
